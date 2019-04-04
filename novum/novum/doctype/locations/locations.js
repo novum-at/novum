eb71f627140bd726ed04d2894fa4046919b5d2a5
Joelios <joel.mesmer@libracore.com>
2019-04-04 11:39:42 +0200

    added room overview in locations

--- novum/novum/doctype/locations/locations.js
+++ novum/novum/doctype/locations/locations.js
@@ -3,6 +3,16 @@
 
 frappe.ui.form.on('Locations', {
 	refresh: function(frm) {
-
+		frappe.call({
+			method: 'novum.novum.doctype.locations.locations.get_room_overview',
+			args: {
+				'location': frm.doc.name
+			},
+			callback: function(r) {
+				if (r.message) {
+					cur_frm.set_df_property('room_overview','options',r.message);
+				}
+			}
+		});
 	}
 });
--- novum/novum/doctype/locations/locations.py
+++ novum/novum/doctype/locations/locations.py
@@ -8,3 +8,23 @@ from frappe.model.document import Document
 
 class Locations(Document):
 	pass
+	
+@frappe.whitelist()
+def get_room_overview(location):
+	room_overview = '<div>'
+	rooms = frappe.db.sql("""SELECT * FROM `tabRooms` WHERE `location` = '{location}'""".format(location=location), as_dict=True)
+	for room in rooms:
+		room_overview = room_overview + '<div class="row"><div class="col-xs-12">'
+		room_overview = room_overview + '<h3>' + room.room_name + '</h3>'
+		room_overview = room_overview + '<p><i class="fa fa-clone"></i> ' + str(room.room_area) + 'm<sup>2</sup></p>'
+		room_overview = room_overview + '<table style="width: 100%;"><tr><th>Equipment</th><th>Fix</th><th>Mobil</th></tr>'
+		equipments = frappe.db.sql("""SELECT * FROM `tabRoomequipment` WHERE `parenttype` = 'Rooms' AND `parentfield` = 'equipment'""", as_dict=True)
+		for equipment in equipments:
+			room_overview = room_overview + '<tr>'
+			if equipment.mobil == 1:
+				room_overview = room_overview + '<td>' + equipment.item + '</td><td><i class="fa fa-times"></i></td><td><i class="fa fa-check"></i></td></tr>'
+			else:
+				room_overview = room_overview + '<td>' + equipment.item + '</td><td><i class="fa fa-check"></i></i></td><td><i class="fa fa-times"></td></tr>'
+		room_overview = room_overview + '</table></div></div>'
+	room_overview = room_overview + '</div>'
+	return room_overview
